af1fa2c06dd2b2d391d49c52c4ef7adaccd0d2b6
Rahatfa <rhtttt123@gmail.com>
2023-01-15 23:54:35 +0600

    done

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.x6hwmbl.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -67,4 +68,3 @@ app.get('/', (req, res)=>{
 app.listen(port, ()=>{
     console.log(`Fit with rahat server running on ${port}`)
 })
-
